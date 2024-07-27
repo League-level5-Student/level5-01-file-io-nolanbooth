568cea29cff697e15f474e57773cf79cf34c4aa5
league <league@imac-16.attlocal.net>
2024-07-27 12:56:53 -0700

    work on just the easy txt part

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,40 @@
 package _01_File_Recorder;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+	
+		
+		try {
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter("thing.txt"));
+			writer.write("noah");
+			writer.write("\n");
+			
+			Scanner scan = new Scanner(System.in);
+			
+			String message = scan.nextLine();
+			
+			
+			writer.write(message);
+			
+			writer.close();
+		
+		}catch(IOException e){
+		e.printStackTrace();		
+				
+			}
+		
+		
+	
+	
+	
+	}
+	
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,13 +1,18 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileEncryptor {
 	/*
-	 * Encryption is the process of encoding a message or information
-	 * in such a way that only authorized parties can access it and
-	 * those who are not authorized cannot.
+	 * Encryption is the process of encoding a message or information in such a way
+	 * that only authorized parties can access it and those who are not authorized
+	 * cannot.
 	 *
-	 * A simple shift cipher works by shifting the letters of a message
-	 * down based on a key. If our key is 4 then:
+	 * A simple shift cipher works by shifting the letters of a message down based
+	 * on a key. If our key is 4 then:
 	 * 
 	 * a b c d e f g h i j k l m n o p q r s t u v w x y z
 	 * 
@@ -17,7 +22,77 @@ public class FileEncryptor {
 	 * 
 	 * "Hello World" changes to "Lipps Asvph"
 	 *
-	 * Create a program that takes a message and a key from the user.
-	 * Use the key to shift each letter in the users input and save the final result to a file.
+	 * Create a program that takes a message and a key from the user. Use the key to
+	 * shift each letter in the users input and save the final result to a file.
 	 */
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number between 0 and 26");
+
+		char[] alphabetArray = new char[26];
+
+		int offset = scan.nextInt() % 26; // modulo should be less than 26 now, at most 25 or something
+
+		// {26} will give char 26 in array
+
+		for (int i = 0; i < alphabetArray.length; i++) {
+			alphabetArray[i] = (char) (i + 97);
+
+		}
+		// System.out.println(alphabetArray.length);
+
+//	for(char c : alphabetArray) {
+//		System.out.println(c);
+//		
+//	}
+
+		char[] scrambleArray = new char[26];
+
+		for (int i = 0; i < alphabetArray.length; i++) {
+			scrambleArray[i] = alphabetArray[(i + offset) % 26];
+
+		}
+		for (char c : scrambleArray) {
+			System.out.println(c);
+		}
+		System.out.println("Enter a message that you want to be encrypted");
+
+		Scanner noah = new Scanner(System.in);
+
+		String message = noah.nextLine();
+
+		StringBuilder bob = new StringBuilder(message);
+
+		for (int i = 0; i < message.length(); i++) {
+			if (message.charAt(i) != ' ') {
+				int dumbthing = Character.getNumericValue(message.charAt(i)) - 10;
+				System.out.println(dumbthing);
+
+				bob.insert(i, scrambleArray[dumbthing]);
+
+			} else {
+
+				bob.insert(i, ' ');
+			}
+		}
+		bob.setLength(bob.length() / 2);
+
+		String finalMessage = bob.toString();
+
+		System.out.println(finalMessage);
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("noah.txt"));
+			
+			writer.write(finalMessage);
+			
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
